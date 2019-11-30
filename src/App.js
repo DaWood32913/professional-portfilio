633d6011c6c8e3c19facd24ce597906df137e8d8
DaWood32913 <woodleyjohnson@yahoo.com>
2019-11-30 14:06:11 -0500

    set up links and routes for navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/Main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,18 +10,19 @@ function App() {
     <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
         <Header transparent title="Title" style={{color: 'white'}}>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/">Home</Link>
+                <Link to="/AboutMe">About Me</Link>
+                <Link to="/Projects">My Projects</Link>
+                <Link to="/Contact">Contact Me</Link>
+                <Link to="/Resume">My Resume</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/">Link</Link>
+                <Link to="/">Link</Link>
+                <Link to="/">Link</Link>
+                <Link to="/">Link</Link>
             </Navigation>
         </Drawer>
         <Content>
