75b07300d803350bb733add54af0e9c0eaa43436
DaWood32913 <woodleyjohnson@yahoo.com>
2019-11-30 12:41:43 -0500

    Created Woodley Component

---
+++ src/components/Woodley.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Woodley from './components/Woodley';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Woodley />
+    </div>
+  );
+}
+
+export default App;
