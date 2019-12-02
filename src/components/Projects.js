85c05c37f2b4083e586ddc46812e27794f791bd3
DaWood32913 <woodleyjohnson@yahoo.com>
2019-12-01 21:14:20 -0500

    added 1st project ro project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,17 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardMenu, CardActions, Button, IconButton, CardText } from 'react-mdl';
-
+import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardMenu,
+  CardActions,
+  Button,
+  IconButton,
+  CardText
+} from "react-mdl";
 
 class Projects extends Component {
   constructor(props) {
@@ -9,99 +20,144 @@ class Projects extends Component {
   }
 
   toggleCategories() {
-    if(this.state.activeTab === 0) {
+    if (this.state.activeTab === 0) {
       return (
         <div className="projects-grid">
           {/* Project 1 */}
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-          <CardTitle style={{color: '#ccc', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV38RNP5zRnhAFQSBWerqdKqwEuVQnTpRK0GPf-mfB9Q5xwGxz&s) center / cover'}}>React Project</CardTitle>
-          <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia...
-          </CardText>
-          <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>CodePen</Button>
-            <Button colored>LiveDemo</Button>
-          </CardActions>
-          <CardMenu style={{color: '#fff'}}>
-            <IconButton name="share" />
-          </CardMenu>
-        </Card>
-        {/* Project 2 */}
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-          <CardTitle style={{color: '#ccc', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV38RNP5zRnhAFQSBWerqdKqwEuVQnTpRK0GPf-mfB9Q5xwGxz&s) center / cover'}}>React Project</CardTitle>
-          <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia...
-          </CardText>
-          <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>CodePen</Button>
-            <Button colored>LiveDemo</Button>
-          </CardActions>
-          <CardMenu style={{color: '#fff'}}>
-            <IconButton name="share" />
-          </CardMenu>
-        </Card>
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#ccc",
+                height: "176px",
+                background:
+                  "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV38RNP5zRnhAFQSBWerqdKqwEuVQnTpRK0GPf-mfB9Q5xwGxz&s) center / cover"
+              }}
+            >
+              Curate
+            </CardTitle>
+            <CardText>
+              Our app lets you search and save some of your favorite art based
+              on a search for an artist’s name. Users can create a user account
+              and save them to their profile to access them later. Users can
+              also delete them if they no longer want to keep them. Just create
+              a profile to get started!
+            </CardText>
+            <CardActions border>
+              <Button colored>
+                <a href="https://curateapp-upenn.herokuapp.com/">Live Demo</a>
+              </Button>
+              <Button colored>
+                <a href="https://github.com/yutsukushi/Curate">GitHub</a>
+              </Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#ccc",
+                height: "176px",
+                background:
+                  "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV38RNP5zRnhAFQSBWerqdKqwEuVQnTpRK0GPf-mfB9Q5xwGxz&s) center / cover"
+              }}
+            >
+              React Project
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
 
-        {/* Project 3 */}
-        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-          <CardTitle style={{color: '#ccc', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV38RNP5zRnhAFQSBWerqdKqwEuVQnTpRK0GPf-mfB9Q5xwGxz&s) center / cover'}}>React Project</CardTitle>
-          <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia...
-          </CardText>
-          <CardActions border>
-            <Button colored>GitHub</Button>
-            <Button colored>CodePen</Button>
-            <Button colored>LiveDemo</Button>
-          </CardActions>
-          <CardMenu style={{color: '#fff'}}>
-            <IconButton name="share" />
-          </CardMenu>
-        </Card>
+          {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "#ccc",
+                height: "176px",
+                background:
+                  "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV38RNP5zRnhAFQSBWerqdKqwEuVQnTpRK0GPf-mfB9Q5xwGxz&s) center / cover"
+              }}
+            >
+              React Project
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              sagittis pellentesque lacus eleifend lacinia...
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
         </div>
-
-      
-      )  
+      );
     } else if (this.state.activeTab === 1) {
       return (
-        <div><h1>This is MySql</h1></div>
-      )
+        <div>
+          <h1>This is MySql</h1>
+        </div>
+      );
     } else if (this.state.activeTab === 2) {
       return (
-        <div><h1>This is MongoDB</h1></div>
-      )
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      );
     } else if (this.state.activeTab === 3) {
       return (
-        <div><h1>This is JavaScript</h1></div>
-      )
+        <div>
+          <h1>This is JavaScript</h1>
+        </div>
+      );
     } else if (this.state.activeTab === 4) {
       return (
-        <div><h1>This is AJAX</h1></div>
-      )
+        <div>
+          <h1>This is AJAX</h1>
+        </div>
+      );
     }
   }
 
-  
-    render() {
-        return(
-          <div className="category-tabs">
-            <Tabs activeTab={this.state.activeTab} onChange={(tabID) => this.setState({ activeTab: tabID})} ripple>
-              <Tab>React</Tab>
-              <Tab>MySql</Tab>
-              <Tab>MongoDB</Tab>
-              <Tab>JavaScript</Tab>
-              <Tab>AJAX</Tab>
-            </Tabs>
+  render() {
+    return (
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabID => this.setState({ activeTab: tabID })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>MySql</Tab>
+          <Tab>MongoDB</Tab>
+          <Tab>JavaScript</Tab>
+          <Tab>AJAX</Tab>
+        </Tabs>
 
-              <Grid>
-                <Cell col={12}>
-                  <div className="content">{this.toggleCategories()}</div>
-                </Cell>
-              </Grid>
-            
-          </div>  
-        )
-    }
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Projects;
